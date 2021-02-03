7b3f6e0cac095a9dae7c5779ca912758df43d55b
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-02-03 22:01:29 +1100

    Update coreStyles.js

--- assets/jss/coreStyles.js
+++ assets/jss/coreStyles.js
@@ -1,5 +1,5 @@
 import Colours from "../strings/colours";
-import {blackColor, hexToRgb, whiteColor} from "./nextjs-material-kit-pro";
+import {whiteColor} from "./nextjs-material-kit-pro";
 
 const extraSmallFont = {
     fontSize: "calc(16px + (20 - 16) * ((100vw - 300px) / (3000 - 300)))"
@@ -7,10 +7,6 @@ const extraSmallFont = {
 
 const smallFont = {
     fontSize: "calc(16px + (24 - 10) * ((100vw - 300px) / (3000 - 300)))"
-}
-
-const mediumFont = {
-    fontSize: "calc(22px + (40 - 22) * ((100vw - 300px) / (3000 - 300)))"
 }
 
 const largeFont = {
@@ -35,25 +31,6 @@ const parallaxSubHeaderText = {
     textAlign: "center",
 }
 
-const sectionText = {
-    color: Colours.primary,
-    margin: "0",
-    textTransform: "uppercase",
-    textAlign: "center",
-}
-
-const sectionHeaderText = {
-    ...mediumFont,
-    ...sectionText,
-    fontWeight: "400",
-}
-
-const sectionSubHeaderText = {
-    ...smallFont,
-    ...sectionText,
-    fontWeight: "350",
-}
-
 const sectionHeight = {
     minHeight: "55vh"
 }
@@ -67,14 +44,10 @@ const greenHrThick = {
 export {
     extraSmallFont,
     smallFont,
-    mediumFont,
     largeFont,
 
     parallaxHeaderText,
     parallaxSubHeaderText,
-
-    sectionHeaderText,
-    sectionSubHeaderText,
 
     sectionHeight,
 
