031c5d8420efc252d275fdfdd9a502e0227b9d40
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-03-03 15:55:30 +1100

    Update header.js

--- assets/data/components/header.js
+++ assets/data/components/header.js
@@ -6,12 +6,13 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import {MAIL_LINK, TEL_LINK, URLS} from "../../strings/urls";
 import Colours from "../../strings/colours";
+import {phone} from "../global";
 
 export const HeaderData = [
     {
-        href: TEL_LINK,
+        href: '',
         icon: <PhoneIcon style={{color: Colours.grayColor[1]}} />,
-        tip: "Mobile",
+        tip: phone,
     },
     {
         href: MAIL_LINK,
