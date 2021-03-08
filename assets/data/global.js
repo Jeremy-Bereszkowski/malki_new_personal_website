dbe591e637a67ba5043c895b9333595a75e8249b
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-03-08 19:44:01 +1100

    Phone

--- assets/data/global.js
+++ assets/data/global.js
@@ -3,7 +3,7 @@ const title = "Counsellor & Family Therapist";
 
 const headerString = `${fullName} | ${title}`;
 
-const phone = "(+61) 0403 110 899";
+const phone = "(+61)403110899";
 const email = "malki.new@gmail.com";
 
 export {
