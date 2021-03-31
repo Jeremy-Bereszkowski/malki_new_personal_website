7ccc31b9e1f6096b70aa1f4ba6a70ab621bdf40b
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-03-31 12:26:28 +1100

    Footer Phone String Hotfix 2

--- components/Links/ExternalLink.js
+++ components/Links/ExternalLink.js
@@ -6,6 +6,7 @@ import { Grid } from "@material-ui/core";
 
 import Colours from "assets/strings/colours";
 import { MAIL_LINK, TEL_LINK } from "../../assets/strings/urls";
+import {phone_string} from "../../assets/data/global";
 
 const useStyles = makeStyles({
     spacing: {
@@ -24,7 +25,9 @@ export default function DataLink(props) {
     const {data, label, mobile, mail} = props
     const classes = useStyles()
 
-    const href = mail ? MAIL_LINK : mobile ? TEL_LINK : data
+    const href = mail ? MAIL_LINK : (mobile ? TEL_LINK : data)
+
+    const dataString = mobile ? phone_string : data
 
     return (
         <Grid
@@ -41,7 +44,7 @@ export default function DataLink(props) {
             <Grid item>
                 <a href={href}>
                     <h4 className={classes.footerBrand}>
-                        {data}
+                        {dataString}
                     </h4>
                 </a>
             </Grid>
